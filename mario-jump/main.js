5df72bb930b32ce374e961f1a0370b4cfaef4c75
Thais Lopes <thaisnandalopes@gmail.com>
2023-10-06 05:16:28 -0300

    Add files via upload

---
+++ mario-jump/main.js
@@ -0,0 +1,32 @@
+const mario = document.querySelector('.mario');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+console.log(marioPosition);
+
+if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    }
+
+}, 10);
+
+document.addEventListener ('keydown', jump);
